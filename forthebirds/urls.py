5c525739f1dafed459e039849e55a63186b5090e
Katherine Erickson <katherine.erickson@gmail.com>
2014-08-11 18:38:35 -0400

    get extremely basic homepage up

--- forthebirds/urls.py
+++ forthebirds/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'forthebirds.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
+    url(r'', include('website.urls')),
+    # url(r'', include('birds.urls')),
+    # url(r'', include('creations.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- website/views.py
+++ website/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
-# Create your views here.
+
+def home(request):
+    return render_to_response('home.html', {},
+                              context_instance=RequestContext(request))
