21b033bc229db17c65b8dda4c0798c0deb5dcd8e
Katherine Erickson <katherine.erickson@gmail.com>
2014-08-23 12:55:05 -0400

    click events for birds

--- birds/static/js/birds.js
+++ birds/static/js/birds.js
@@ -3,8 +3,14 @@ $(document).ready(function() {
 });
 
 createExpandButtons = function() {
-  $(".plus-sign").closest("li").on("click", function() {
+  links = $(".plus-sign").closest("a");
+
+  links.css("cursor", "pointer");
+
+  links.on("click", function(e) {
+    e.preventDefault();
+    $(this).toggleClass("active");
     $(this).find(".plus-sign").toggleClass("minus");
-    $(this).next("ul").toggleClass("collapsed");
+    $(this).closest("li").next("ul").toggleClass("collapsed");
   });
 }
