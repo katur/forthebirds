8b6e41d2bbe41bda496482a672a1a0c255139ee8
Katherine Erickson <katherine.erickson@gmail.com>
2014-08-25 17:45:16 -0400

    unit test setup (sqlite for unit testing)

--- birds/tests.py
+++ birds/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from birds.models import Species
+
+
+class BirdsTestCase(TestCase):
+    def setUp(self):
+        Species.objects.create(
+
+    def test_one_plus_one(self):
+        self.assertTrue(2 == 1 + 1)
--- manage.py
+++ manage.py
@@ -7,4 +7,7 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
+    if 'test' in sys.argv:
+        DATABASE_ENGINE = 'sqlite3'
+
     execute_from_command_line(sys.argv)
