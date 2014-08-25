79731c50ca02943cdde049fdf18d965de6a0d1fc
Katherine Erickson <katherine.erickson@gmail.com>
2014-08-25 14:05:00 -0400

    enable static media serving with dj-static.

--- forthebirds/wsgi.py
+++ forthebirds/wsgi.py
@@ -11,5 +11,5 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forthebirds.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-application = Cling(get_wsgi_application())
+from dj_static import Cling, MediaCling
+application = Cling(MediaCling(get_wsgi_application()))
