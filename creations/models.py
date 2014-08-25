3f1dac479c21f63f1ff5afa42efe061e6f614a49
Katherine Erickson <katherine.erickson@gmail.com>
2014-08-25 18:19:01 -0400

    make purchase url optional for books

--- creations/models.py
+++ creations/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100)
     year_published = models.PositiveSmallIntegerField(null=True, blank=True)
-    purchase_url = models.CharField(max_length=500)
+    purchase_url = models.CharField(max_length=500, blank=True)
     description = models.TextField(blank=True)
     photo = models.ImageField(null=True, blank=True, upload_to='books')
 
