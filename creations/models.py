c3e81f8a8e682d649728181862d54fb6584d832f
Katherine Erickson <katherine.erickson@gmail.com>
2014-08-25 13:12:34 -0400

    add book model

--- creations/models.py
+++ creations/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    date_published = models.DateField(null=True, blank=True)
+    purchase_url = models.CharField(max_length=500)
+    description = models.TextField(blank=True)
