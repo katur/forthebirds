f7183369f1f64587613ae51a5cd028d1c8dcb4a4
Katherine Erickson <katherine.erickson@gmail.com>
2014-08-26 12:22:50 -0400

    add radio program page

--- creations/urls.py
+++ creations/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'creations.views',
     url(r'^books$', 'books', name='books_url'),
+    url(r'^radio$', 'radio', name='radio_url'),
 )
--- creations/views.py
+++ creations/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from creations.models import Book
+from creations.models import Book, RadioProgram
 
 
 def books(request):
@@ -11,4 +11,14 @@ def books(request):
     }
 
     return render_to_response('books.html', template_dictionary,
+                              context_instance=RequestContext(request))
+
+
+def radio(request):
+    programs = RadioProgram.objects.all()
+    template_dictionary = {
+        'programs': programs,
+    }
+
+    return render_to_response('radio.html', template_dictionary,
                               context_instance=RequestContext(request))
