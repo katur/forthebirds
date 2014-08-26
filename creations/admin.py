293ad0a3a2151de8054879f158b484fc1bb1d302
Katherine Erickson <katherine.erickson@gmail.com>
2014-08-26 12:24:30 -0400

    radio program admin

--- creations/admin.py
+++ creations/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from creations.models import Book
+from creations.models import Book, RadioProgram
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -10,4 +10,12 @@ class BookAdmin(admin.ModelAdmin):
     )
 
 
+class RadioProgramAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'original_air_date',
+    )
+
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(RadioProgram, RadioProgramAdmin)
