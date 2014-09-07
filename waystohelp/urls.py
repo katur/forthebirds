d4dd45b5ac4c5eac9fb855cbe3c9f0e07d1204ff
Katherine Erickson <katherine.erickson@gmail.com>
2014-09-07 16:07:45 -0400

    change urls for 101 ways to help birds pages

--- waystohelp/urls.py
+++ waystohelp/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'waystohelp.views',
-    url(r'^101waystohelpbirds$', 'ways_to_help', name='waystohelp_url'),
-    url(r'^waytohelpbirds/(?P<id>.+)$', 'way_to_help', name='waytohelp_url'),
+    url(r'^helpbirds$', 'ways_to_help', name='waystohelp_url'),
+    url(r'^helpbirds/(?P<id>.+)$', 'way_to_help', name='waytohelp_url'),
 )
