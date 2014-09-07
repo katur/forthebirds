d2e065ceb3fa760afd488c05bda6ad979150ad1a
Katherine Erickson <katherine.erickson@gmail.com>
2014-09-07 17:05:12 -0400

    add primary photo to user profile

--- website/models.py
+++ website/models.py
@@ -8,6 +8,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, primary_key=True)
     about = models.TextField('Text for About page',
                              help_text=MARKDOWN_PROMPT, blank=True)
+    primary_photo = models.FileField(null=True, blank=True,
+                                     upload_to='photos_of_laura')
 
     def __unicode__(self):
         return self.user.get_full_name()
