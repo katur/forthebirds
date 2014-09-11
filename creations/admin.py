33330326914546910dba2ca731321e1b96f20a97
Katherine Erickson <katherine.erickson@gmail.com>
2014-09-11 15:26:11 -0400

    admin layout

--- creations/admin.py
+++ creations/admin.py
@@ -3,12 +3,29 @@ from django.contrib import admin
 from creations.models import Book, RadioProgram
 
 
+creation_id_fields = ('title', 'description',)
+creation_tagging_fields = ('species', 'tags',)
+
+
 class BookAdmin(admin.ModelAdmin):
     list_display = (
         'title',
         'year_published',
     )
     filter_horizontal = ('species',)
+    fieldsets = (
+        (None, {
+            'fields': creation_id_fields
+        }),
+        ('Publishing Info', {
+            'fields': ('publisher', 'isbn_10', 'isbn_13', 'date_published',
+                       'purchase_url', 'photo',),
+            'classes': ('collapse',),
+        }),
+        ('Tagging', {
+            'fields': creation_tagging_fields
+        }),
+    )
 
 
 class RadioProgramAdmin(admin.ModelAdmin):
@@ -17,6 +34,19 @@ class RadioProgramAdmin(admin.ModelAdmin):
         'original_air_date',
     )
     filter_horizontal = ('species',)
+    fieldsets = (
+        (None, {
+            'fields': creation_id_fields
+        }),
+        ('Program Details', {
+            'fields': ('original_air_date', 'supplemental_content_url',
+                       'transcript', 'file',),
+            'classes': ('collapse',),
+        }),
+        ('Tagging', {
+            'fields': creation_tagging_fields
+        }),
+    )
 
 
 admin.site.register(Book, BookAdmin)
