426c0c4b6a7f18149c446d6928b0cff4af42a9c5
katur <katherine.erickson@gmail.com>
2014-09-11 18:59:29 +0000

    make creations migration regarding tagging on production server so timestamps match dev

---
+++ creations/migrations/0002_auto_20140911_1457.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import taggit.managers
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('taggit', '0002_auto_20140911_1457'),
+        ('creations', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='creation',
+            name='tags',
+            field=taggit.managers.TaggableManager(to='taggit.Tag', through='taggit.TaggedItem', blank=True, help_text='A comma-separated list of tags.', verbose_name='Tags'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='creation',
+            name='species',
+            field=models.ManyToManyField(to=b'birds.Species', blank=True),
+        ),
+    ]
