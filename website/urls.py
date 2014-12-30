34bc3bf9b039f28e618e324b28a2ec06d8b9d579
Katherine Erickson <katherine.erickson@gmail.com>
2014-12-30 14:08:08 -0600

    move About Laura to home; more header style work

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'website.views',
     url(r'^$', 'home', name='home_url'),
-    url(r'^about-laura$', 'about', name='about_url'),
 )
 
 urlpatterns += patterns(
