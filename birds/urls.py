09769018ec54dfb487b6771919ccc80941a0f646
Katherine Erickson <katherine.erickson@gmail.com>
2016-01-12 13:16:20 -0500

    Minor code format change

--- birds/urls.py
+++ birds/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns(
     'birds.views',
     url(r'^birds/$', 'birds', name='birds_url'),
     url(r'^bird/(?P<id>\d+)/$', 'bird', name='bird_url'),
-    url(r'^minnesota-birds/$', 'minnesota_birds', name='minnesota_birds_url'),
+    url(r'^minnesota-birds/$', 'minnesota_birds',
+        name='minnesota_birds_url'),
 )
