07c129f6dc35583237537fe3f9d7ee138d703d25
Katherine Erickson <katherine.erickson@gmail.com>
2016-01-11 21:53:38 -0500

    Add trailing slash to urls to take advantage of APPEND_SLASH setting

--- birds/urls.py
+++ birds/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'birds.views',
-    url(r'^birds$', 'birds', name='birds_url'),
-    url(r'^bird/(?P<id>.+)$', 'bird', name='bird_url'),
-    url(r'^minnesota-birds$', 'minnesota_birds', name='minnesota_birds_url'),
+    url(r'^birds/$', 'birds', name='birds_url'),
+    url(r'^bird/(?P<id>.+)/$', 'bird', name='bird_url'),
+    url(r'^minnesota-birds/$', 'minnesota_birds', name='minnesota_birds_url'),
 )
--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'website.views',
     url(r'^$', 'home', name='home_url'),
-    url(r'^about-laura$', 'about', name='about_url'),
+    url(r'^about-laura/$', 'about', name='about_url'),
 )
 
 urlpatterns += patterns(
