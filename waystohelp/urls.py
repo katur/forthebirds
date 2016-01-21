450c0f5c266b92a215aa12b1f38368fdd4632076
Katherine Erickson <katherine.erickson@gmail.com>
2016-01-21 18:26:02 -0500

    Consistently use underscores in named urls

--- waystohelp/urls.py
+++ waystohelp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^help-birds/$', views.ways_to_help, name='waystohelp_url'),
+    url(r'^help-birds/$', views.ways_to_help, name='ways_to_help_url'),
     url(r'^help-birds/(?P<id>\d+)/$', views.way_to_help,
-        name='waytohelp_url'),
+        name='way_to_help_url'),
 ]
