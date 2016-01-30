2c8bd11d79beb1629d0c31f2fa15cdae2acfceb9
Katherine Erickson <katherine.erickson@gmail.com>
2016-01-29 21:46:55 -0500

    Change waystohelp urls to ways-to-help, way-to-help

--- waystohelp/urls.py
+++ waystohelp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^help-birds/$', views.ways_to_help, name='ways_to_help_url'),
-    url(r'^help-birds/(?P<id>\d+)/$', views.way_to_help,
+    url(r'^ways-to-help/$', views.ways_to_help, name='ways_to_help_url'),
+    url(r'^way-to-help/(?P<id>\d+)/$', views.way_to_help,
         name='way_to_help_url'),
 ]
