bc3182d55b65b0518273728d3a798562a11e9fe8
Michael P. Geraci <me@mgeraci.com>
2016-01-30 21:17:58 -0500

    add some functions from other libs, load images lazily

--- birds/static/js/birds.coffee
+++ birds/static/js/birds.coffee
@@ -1,7 +1,12 @@
 $(document).ready ->
-  if $("body").attr("id") == "birds"
+  page = $("body").attr("id")
+
+  if page == "birds"
     expandNoSubfamilies()
     createExpandButtons()
+
+  if page == "photo-checklist"
+    PhotoChecklist.init()
 
 
 expandNoSubfamilies = ->
@@ -18,3 +23,110 @@ createExpandButtons = ->
     $(this).toggleClass("active")
     $(this).find(".plus-sign").toggleClass("minus")
     $(this).closest("li").next("ul").toggleClass("collapsed")
+
+
+# onScreen jQuery plugin v0.2.1
+# (c) 2011-2013 Ben Pickles
+#
+# http://benpickles.github.io/onScreen
+#
+# Released under MIT license.
+(($) ->
+
+  $.expr[':'].onScreen = (elem) ->
+    $window = $(window)
+    viewport_top = $window.scrollTop()
+    viewport_height = $window.height()
+    viewport_bottom = viewport_top + viewport_height
+    $elem = $(elem)
+    top = $elem.offset().top
+    height = $elem.height()
+    bottom = top + height
+    top >= viewport_top and top < viewport_bottom or bottom > viewport_top and bottom <= viewport_bottom or height > viewport_height and top <= viewport_top and bottom >= viewport_bottom
+
+  return
+) jQuery
+
+
+# now and throttle taken from underscore.js
+_ = {}
+_.now = Date.now or ->
+  (new Date).getTime()
+
+_.throttle = (func, wait, options) ->
+  context = undefined
+  args = undefined
+  result = undefined
+  timeout = null
+  previous = 0
+  if !options
+    options = {}
+
+  later = ->
+    previous = if options.leading == false then 0 else _.now()
+    timeout = null
+    result = func.apply(context, args)
+    if !timeout
+      context = args = null
+    return
+
+  ->
+    now = _.now()
+    if !previous and options.leading == false
+      previous = now
+    remaining = wait - (now - previous)
+    context = this
+    args = arguments
+    if remaining <= 0 or remaining > wait
+      if timeout
+        clearTimeout timeout
+        timeout = null
+      previous = now
+      result = func.apply(context, args)
+      if !timeout
+        context = args = null
+    else if !timeout and options.trailing != false
+      timeout = setTimeout(later, remaining)
+    result
+
+
+window.PhotoChecklist = {
+  classnames: {
+    unloaded: "photo-and-caption--unloaded"
+    loading: "photo-and-caption--loading"
+    loaded: "photo-and-caption--loaded"
+  }
+
+  init: ->
+    lazyScroll = _.throttle(@checkScroll.bind(@), 500)
+    $(document).on("scroll", (e) =>
+      lazyScroll()
+    )
+
+    # load the first set before scroll
+    @checkScroll()
+
+  checkScroll: ->
+    # scoping in here is a little whack: because we want to use `this` for
+		# the jQuery each loop, refer to the singleton for triggerLoad
+    $(".photo-and-caption--unloaded:onScreen").each( ->
+      PhotoChecklist.triggerLoad($(@))
+    )
+
+  triggerLoad: (el) ->
+    return if el.hasClass(@classnames.loaded)
+    return if el.hasClass(@classnames.loading)
+    console.log "loading", @
+
+    el
+      .removeClass(@classnames.unloaded)
+      .addClass(@classnames.loading)
+
+    img = new Image()
+
+    $(img).on("load", =>
+      el
+        .removeClass(@classnames.loading)
+        .prepend(img)
+    ).attr("src", el.attr("data-src"))
+}
--- birds/static/js/birds.js
+++ birds/static/js/birds.js
@@ -1,10 +1,15 @@
 (function() {
-  var createExpandButtons, expandNoSubfamilies;
+  var _, createExpandButtons, expandNoSubfamilies;
 
   $(document).ready(function() {
-    if ($("body").attr("id") === "birds") {
+    var page;
+    page = $("body").attr("id");
+    if (page === "birds") {
       expandNoSubfamilies();
-      return createExpandButtons();
+      createExpandButtons();
+    }
+    if (page === "photo-checklist") {
+      return PhotoChecklist.init();
     }
   });
 
@@ -22,6 +27,111 @@
       $(this).find(".plus-sign").toggleClass("minus");
       return $(this).closest("li").next("ul").toggleClass("collapsed");
     });
+  };
+
+  (function($) {
+    $.expr[':'].onScreen = function(elem) {
+      var $elem, $window, bottom, height, top, viewport_bottom, viewport_height, viewport_top;
+      $window = $(window);
+      viewport_top = $window.scrollTop();
+      viewport_height = $window.height();
+      viewport_bottom = viewport_top + viewport_height;
+      $elem = $(elem);
+      top = $elem.offset().top;
+      height = $elem.height();
+      bottom = top + height;
+      return top >= viewport_top && top < viewport_bottom || bottom > viewport_top && bottom <= viewport_bottom || height > viewport_height && top <= viewport_top && bottom >= viewport_bottom;
+    };
+  })(jQuery);
+
+  _ = {};
+
+  _.now = Date.now || function() {
+    return (new Date).getTime();
+  };
+
+  _.throttle = function(func, wait, options) {
+    var args, context, later, previous, result, timeout;
+    context = void 0;
+    args = void 0;
+    result = void 0;
+    timeout = null;
+    previous = 0;
+    if (!options) {
+      options = {};
+    }
+    later = function() {
+      previous = options.leading === false ? 0 : _.now();
+      timeout = null;
+      result = func.apply(context, args);
+      if (!timeout) {
+        context = args = null;
+      }
+    };
+    return function() {
+      var now, remaining;
+      now = _.now();
+      if (!previous && options.leading === false) {
+        previous = now;
+      }
+      remaining = wait - (now - previous);
+      context = this;
+      args = arguments;
+      if (remaining <= 0 || remaining > wait) {
+        if (timeout) {
+          clearTimeout(timeout);
+          timeout = null;
+        }
+        previous = now;
+        result = func.apply(context, args);
+        if (!timeout) {
+          context = args = null;
+        }
+      } else if (!timeout && options.trailing !== false) {
+        timeout = setTimeout(later, remaining);
+      }
+      return result;
+    };
+  };
+
+  window.PhotoChecklist = {
+    classnames: {
+      unloaded: "photo-and-caption--unloaded",
+      loading: "photo-and-caption--loading",
+      loaded: "photo-and-caption--loaded"
+    },
+    init: function() {
+      var lazyScroll;
+      lazyScroll = _.throttle(this.checkScroll.bind(this), 500);
+      $(document).on("scroll", (function(_this) {
+        return function(e) {
+          return lazyScroll();
+        };
+      })(this));
+      return this.checkScroll();
+    },
+    checkScroll: function() {
+      return $(".photo-and-caption--unloaded:onScreen").each(function() {
+        return PhotoChecklist.triggerLoad($(this));
+      });
+    },
+    triggerLoad: function(el) {
+      var img;
+      if (el.hasClass(this.classnames.loaded)) {
+        return;
+      }
+      if (el.hasClass(this.classnames.loading)) {
+        return;
+      }
+      console.log("loading", this);
+      el.removeClass(this.classnames.unloaded).addClass(this.classnames.loading);
+      img = new Image();
+      return $(img).on("load", (function(_this) {
+        return function() {
+          return el.removeClass(_this.classnames.loading).prepend(img);
+        };
+      })(this)).attr("src", el.attr("data-src"));
+    }
   };
 
 }).call(this);
