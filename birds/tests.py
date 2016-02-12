1b60c78ddebc0a722189fd100ade53d0d4e732e8
Katherine Erickson <katherine.erickson@gmail.com>
2016-02-12 18:45:49 -0500

    Remove bogus broken code in not-started tests

--- birds/tests.py
+++ birds/tests.py
@@ -1,11 +1,6 @@
 from django.test import TestCase
 
-from birds.models import Species
-
 
 class BirdsTestCase(TestCase):
-    def setUp(self):
-        Species.objects.create(
-
     def test_one_plus_one(self):
         self.assertTrue(2 == 1 + 1)
