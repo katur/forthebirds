7ad352eda3f8096214484776fcf600c72b57bb4a
Katherine Erickson <katherine.erickson@gmail.com>
2016-02-17 17:58:35 -0500

    Clean up coffee-compiled js

--- waystohelp/static/js/waystohelp.js
+++ waystohelp/static/js/waystohelp.js
@@ -1,71 +1,73 @@
-(function() {
-  var generateBackgroundImagesForWays, generateRandomBackgroundColors, getHashCode, getHexableHash, numHexColors;
+$(document).ready(function() {
+  if ($("body").attr("id") === "ways-to-help") {
+    generateRandomBackgroundColors();
+    generateBackgroundImagesForWays();
+  }
+});
 
-  $(document).ready(function() {
-    if ($("body").attr("id") === "ways-to-help") {
-      generateRandomBackgroundColors();
-      return generateBackgroundImagesForWays();
-    }
-  });
 
-  generateBackgroundImagesForWays = function() {
-    var backgroundImage, j, len1, results, way, ways;
-    ways = $(".way-to-help-card");
-    results = [];
-    for (j = 0, len1 = ways.length; j < len1; j++) {
-      way = ways[j];
-      way = $(way);
-      backgroundImage = way.attr("data-background");
-      results.push(way.css("background-image", "url(" + backgroundImage + ")"));
-    }
-    return results;
-  };
+function generateRandomBackgroundColors() {
+  var elements = $(".random-background-color");
+  var results = [];
 
-  getHashCode = function(str) {
-    var char, code, hash, hashPrev, j, len, len1;
-    hash = 0;
-    len = str.length;
-    if (len === 0) {
-      return hash;
-    }
-    for (j = 0, len1 = str.length; j < len1; j++) {
-      char = str[j];
-      if (char.trim() === "") {
-        continue;
-      }
-      code = char.charCodeAt(0);
-      hash = ((hash << 5) - hash) + code;
-      hashPrev = hash;
-      hash |= 0;
-    }
+  var i, element, hash, hexString;
+  for (i = 0; i < elements.length; i++) {
+    element = $(elements[i]);
+    hash = getHexableHash(element.text());
+    hexString = hash.toString(16);
+    results.push(element.css("background-color", "#" + hexString));
+  }
+
+  return results;
+};
+
+
+const NUM_HEX_COLORS = Math.pow(2, 4 * 6);
+
+
+function getHexableHash(str) {
+  var hash = getHashCode(str);
+  var r = hash % NUM_HEX_COLORS;
+
+  if (r < 0) {
+    r *= -1;
+  }
+
+  return r;
+};
+
+
+function getHashCode(str) {
+  var hash = 0;
+
+  if (str.length === 0) {
     return hash;
-  };
+  }
 
-  numHexColors = Math.pow(2, 4 * 6);
-
-  getHexableHash = function(str) {
-    var hash, i;
-    hash = getHashCode(str);
-    i = hash % numHexColors;
-    if (i < 0) {
-      i *= -1;
+  var i, ch;
+  for (i = 0; i < str.length; i++) {
+    ch = str[i];
+    if (ch.trim() === "") {
+      continue;
     }
-    return i;
-  };
+    hash = ((hash << 5) - hash) + ch.charCodeAt(0);
+    hash |= 0;
+  }
 
-  generateRandomBackgroundColors = function() {
-    var element, elements, hash, hexString, j, len1, results, text;
-    elements = $(".random-background-color");
-    results = [];
-    for (j = 0, len1 = elements.length; j < len1; j++) {
-      element = elements[j];
-      element = $(element);
-      text = element.text();
-      hash = getHexableHash(text);
-      hexString = hash.toString(16);
-      results.push(element.css("background-color", "#" + hexString));
-    }
-    return results;
-  };
+  return hash;
+};
 
-}).call(this);
+
+function generateBackgroundImagesForWays() {
+  var ways = $(".way-to-help-card");
+  var results = [];
+
+  var i, j, way, backgroundImage;
+  for (i = 0, j = ways.length; i < j; i++) {
+    way = $(ways[i]);
+    backgroundImage = way.attr("data-background");
+    results.push(way.css("background-image", "url(" + backgroundImage + ")"));
+  }
+
+  return results;
+};
