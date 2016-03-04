35b820b1e3b80ddd871dc66c3a9838c1909be2b7
Katherine Erickson <katherine.erickson@gmail.com>
2016-03-04 12:28:35 -0500

    Minor copy change

--- creations/management/commands/mp3_sanity_check.py
+++ creations/management/commands/mp3_sanity_check.py
@@ -11,17 +11,19 @@ class Command(BaseCommand):
     def handle(self, **options):
         for p in RadioProgram.objects.all():
             if not _is_properly_named('radio', p.file.name):
-                self.stderr.write('IMPROPER: {}'.format(p.file.name))
+                self.stderr.write('IMPROPER FILENAME: {}'.format(p.file.name))
 
             if not _dates_match(p.file.name, p.air_date):
-                self.stderr.write('DATE MISMATCH: {}'.format(p.file.name))
+                self.stderr.write('DATE MISMATCH: {} (air date {})'.format(
+                    p.file.name, p.air_date))
 
         for s in SoundRecording.objects.all():
             if not _is_properly_named('soundrecordings', s.file.name):
                 self.stderr.write('IMPROPER: {}'.format(s.file.name))
 
             if not _dates_match(s.file.name, s.date_recorded):
-                self.stderr.write('DATE MISMATCH: {}'.format(s.file.name))
+                self.stderr.write('DATE MISMATCH: {} (air date {})'.format(
+                    p.file.name, p.air_date))
 
 
 def _is_properly_named(directory, filename):
