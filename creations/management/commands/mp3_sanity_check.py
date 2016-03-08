a60703360e913e9433bc4049043d554fa0a709dd
Katherine Erickson <katherine.erickson@gmail.com>
2016-03-08 03:23:41 +0000

    Check that file exists in mp3_sanity_check

--- creations/management/commands/mp3_sanity_check.py
+++ creations/management/commands/mp3_sanity_check.py
@@ -1,5 +1,7 @@
+import os.path
 import re
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from creations.models import RadioProgram, SoundRecording
@@ -10,25 +12,30 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         for p in RadioProgram.objects.all():
-            if not _is_properly_named('radio', p.file.name):
-                self.stderr.write('IMPROPER FILENAME: {}'.format(p.file.name))
-
-            if not _dates_match(p.file.name, p.air_date):
-                self.stderr.write('DATE MISMATCH: {} (air date {})'.format(
-                    p.file.name, p.air_date))
+            _perform_sanity_checks(p.file.name, p.air_date, self)
 
         for s in SoundRecording.objects.all():
-            if not _is_properly_named('soundrecordings', s.file.name):
-                self.stderr.write('IMPROPER: {}'.format(s.file.name))
-
-            if not _dates_match(s.file.name, s.date_recorded):
-                self.stderr.write('DATE MISMATCH: {} (air date {})'.format(
-                    p.file.name, p.air_date))
+            _perform_sanity_checks(s.file.name, s.date_recorded, self)
 
 
-def _is_properly_named(directory, filename):
-    return re.match(r'^{}/\d\d\d\d\-\d\d\-\d\d_[^_]*$'.format(directory),
-                    filename)
+def _perform_sanity_checks(filename, date, stream):
+    if not _file_exists(filename):
+        stream.stderr.write('FILE DOES NOT EXIST: {}'.format(filename))
+
+    if not _is_properly_named(filename):
+        stream.stderr.write('IMPROPER FILENAME: {}'.format(filename))
+
+    if not _dates_match(filename, date):
+        stream.stderr.write('DATE MISMATCH: {} (database says {})'.format(
+            filename, date))
+
+
+def _file_exists(filename):
+    return os.path.isfile(settings.MEDIA_ROOT + '/' + filename)
+
+
+def _is_properly_named(filename):
+    return re.match(r'^\d\d\d\d\-\d\d\-\d\d_[^_]*$', filename.split('/')[1])
 
 
 def _dates_match(filename, date):
