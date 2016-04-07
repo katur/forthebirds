ea1613c46d96394c736f6ab215c274a1eb897cc6
Katherine Erickson <katherine.erickson@gmail.com>
2016-04-07 14:32:49 -0400

    Redo home page slideshow with Unslider

--- website/views.py
+++ website/views.py
@@ -6,10 +6,26 @@ from django.shortcuts import render, get_object_or_404
 from utils.http import http_response_ok
 from website.models import User, UploadedImage
 
+SLIDESHOW_IMAGES = (
+    'https://farm9.staticflickr.com/8377/8443224020_906862b207_b.jpg',
+    'https://farm6.staticflickr.com/5602/15596254576_5216a28c1c_b.jpg',
+    'https://farm4.staticflickr.com/3819/9127344519_9ff6039003_b.jpg',
+    'https://farm3.staticflickr.com/2943/15443275435_f8e6e0ca79_b.jpg',
+    'https://farm9.staticflickr.com/8118/8705949874_b848716501_b.jpg',
+    'https://farm4.staticflickr.com/3126/3128156539_410c815f3c_b.jpg',
+    'https://farm4.staticflickr.com/3446/3904449304_1fef884402_b.jpg',
+    'https://farm5.staticflickr.com/4102/4770673361_f3883fbb77_b.jpg',
+    'https://farm9.staticflickr.com/8537/8697779098_068fe2969a_b.jpg',
+    'https://farm8.staticflickr.com/7285/8733022782_e81d3b137d_b.jpg',
+)
+
 
 def home(request):
     """Render the homepage."""
-    return render(request, 'home.html', {})
+    context = {
+        'slideshow_images': SLIDESHOW_IMAGES,
+    }
+    return render(request, 'home.html', context)
 
 
 def about(request):
