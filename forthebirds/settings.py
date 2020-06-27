85de46da08e3fb8ed60051d98efb06f126a06aae
Michael P. Geraci <me@mgeraci.com>
2020-06-26 23:39:58 -0500

    Add explicit file upload permissions (#17)
    * set explicit file upload permissions
    
    * update the perm from python 3 to 2

--- forthebirds/settings.py
+++ forthebirds/settings.py
@@ -130,6 +130,7 @@ LOGIN_REDIRECT_URL = '/'
 # Miscellany
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_URLS_REGEX = r'^/radio/program-count$'
+FILE_UPLOAD_PERMISSIONS = 0644
 
 MARKDOWN_PROMPT = (
     'Use Markdown syntax for italics, bullets, etc. See '
