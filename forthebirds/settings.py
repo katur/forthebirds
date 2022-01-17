508e721102c55929fc51323e45400632757e14e9
Katherine Erickson <katherine.erickson@gmail.com>
2022-01-17 17:39:43 -0600

    Update to default Django 2.2 middleware

--- forthebirds/settings.py
+++ forthebirds/settings.py
@@ -64,8 +64,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = (
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
